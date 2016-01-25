a20b7db6099d0ad701ac8e61d99bb2d70fd2780b
Rachel Lim <rachel.y.lim@gmail.com>
2016-01-25 08:33:22 -0500

    create player models; implement ability to place x and o.

---
+++ app.js
@@ -0,0 +1,80 @@
+// player O 
+// player X 
+// place things in the right spot 
+// keep track of which player is playing
+// UI draggable 
+
+// when you click on a spot, if it is playerO, place an O
+// if player O just played, player X is playing 
+
+ 
+var turn = true; 
+
+var playerO = { 
+  play: function(spot){ 
+    $(spot).html("O");
+  },
+  positions: [],
+};
+
+var playerX = { 
+  play: function(spot){ 
+    $(spot).html("X");
+  },
+  positions: [],
+};
+
+var checkWinner = function(playerPositions){
+  var player = playerPositions.sort().toString(); 
+  // debugger;
+
+  var winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]];
+
+  var won = false;
+
+  for(var i = 0; i < winningCombos.length; i++){
+    // console.log(winningCombos[i].map(Number));
+    if (player.includes((winningCombos[i].map(Number)))){
+      won = true;
+    };
+  };
+  return won;
+};
+
+$( document ).ready(function() {
+
+  $("td").click(function(){
+    if (turn === true) {
+      var id = $(this).attr("id"); 
+      playerO.play(this);
+      playerO.positions.push(id);
+      turn = false;
+      if (checkWinner(playerO.positions)){
+        alert("Player O has won!");
+      };
+    } else if (turn === false) {
+      var id = $(this).attr("id");
+      playerX.play(this);
+      playerX.positions.push(id);
+      turn = true; 
+      if (checkWinner(playerX.positions)){
+        alert("Player X has won!");
+      };
+    };
+  });
+});
+
+//determine if there is a winner 
+//if there are 3 xs in a row or diagonal
+// if there are 3 Os in a row or diagonal
+
+// check if there are at least 3 xs or os 
+// check if xs or os are in a row 
+// check if xs or os are diagonal 
+
+// click on an id 
+// hash, key: 0 or a 1 
+// if value is an O, key: 0 
+// if value is an X, key: 1
+
+// if at any time you have 3 ones,
