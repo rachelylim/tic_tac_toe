400ed2c1b03023d164282a03dd09574dc3aa7842
Rachel Lim <rachel.y.lim@gmail.com>
2016-02-02 16:21:40 -0500

    Add overlay and winner alert.

--- app.js
+++ app.js
@@ -1,43 +1,26 @@
-// player O 
-// player X 
-// place things in the right spot 
-// keep track of which player is playing
-// UI draggable 
-
-// when you click on a spot, if it is playerO, place an O
-// if player O just played, player X is playing 
-
- 
-
-
-$( document ).ready(function() {
-
-  var turn = true; 
+var turn = true; 
 
 var playerO= { 
   play: function(spot){ 
-    $(spot).html("O");
+    $(spot).html('<img src="red.png" alt="O" />');
   },
   positions: [],
 };
 
 var playerX = { 
   play: function(spot){ 
-    $(spot).html("X");
+    $(spot).html('<img src="green.png" alt="X" />');
   },
   positions: [],
 };
 
 var checkWinner = function(playerPositions){
   var player = playerPositions.sort().toString(); 
-  // debugger;
 
   winningCombos = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [3, 5, 7], [1, 4, 7], [2, 5, 8], [3, 6, 9]];
 
   var won = false;
   for(var i = 0; i < winningCombos.length; i++){
-    // console.log(winningCombos[i].map(Number));
-
     if (player.includes((winningCombos[i].map(Number)))){
       won = true;
     };
@@ -51,44 +34,39 @@ var clearBoard = function() {
   playerX.positions = []
 }
 
+var declareWinner = function(player) {
+  // alert(player)
+  var message = player + " wins!"
+  $(".overlay").fadeIn("slow");
+  $("#alert").html(message);
+  $("#alert").show();
+}
+
+$( document ).ready(function() {
   $("td").click(function(){
     if (turn === true) {
       var id = $(this).attr("id"); 
       playerO.play(this);
       playerO.positions.push(id);
       turn = false;
-      // debugger
       if (checkWinner(playerO.positions)){
-        console.log(playerO.positions);
-        alert("Player O has won!");
-
-        clearBoard();
+        declareWinner("Pink Flower");
       };
-    } else if (turn === false) {
+    } else {
       var id = $(this).attr("id");
       playerX.play(this);
       playerX.positions.push(id);
       turn = true; 
       if (checkWinner(playerX.positions)){
-        alert("Player X has won!");
-
-        clearBoard();
+        declareWinner("Green Flower");
       };
     };
   });
+
+  $(".overlay").click(function() {
+    $(this).fadeOut("slow");
+    $("#alert").hide();
+    clearBoard();
+  });
+
 });
-
-//determine if there is a winner 
-//if there are 3 xs in a row or diagonal
-// if there are 3 Os in a row or diagonal
-
-// check if there are at least 3 xs or os 
-// check if xs or os are in a row 
-// check if xs or os are diagonal 
-
-// click on an id 
-// hash, key: 0 or a 1 
-// if value is an O, key: 0 
-// if value is an X, key: 1
-
-// if at any time you have 3 ones,
